d6643f58764e3ee2782ac011d82e50a7ef6cdeae
Ivan <boss.marzan@bk.ru>
2021-07-14 16:01:09 +0300

    factorial v2

--- Program.cpp
+++ Program.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 int fact(int n);
+int iterf(int n);
 
 int main(){
 int n;
 cout << "Enter value for find factorial: " << endl;
 cin >> n;
 cout << n << "! = ";
-cout << fact(n) << endl;
+cout << iterf(n) << endl;
 return 0;
 }
 
@@ -18,4 +19,11 @@ int x;
 if (n==1) return(1);
 x = fact(n-1) * n;
 return (x);
+}
+
+
+int iterf(int n){
+int x = 1;
+for(int i=1;i<=n;i++) x = x * i;
+	return x;
 }
