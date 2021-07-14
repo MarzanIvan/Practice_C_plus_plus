70044396ec12a8910d3d6c3cd5595d59eeb467fb
Ivan <boss.marzan@bk.ru>
2021-07-14 13:55:43 +0300

    function arguments pointer

--- Program.cpp
+++ Program.cpp
@@ -3,16 +3,31 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]){
-int last;
-for (int i=1;i<argc;i++){
-last = strlen(argv[i]);
-while(last>-1){
-cout << argv[i][last] << ' ';
-last--;
-}
-cout << '\n';
-}
+char *f(char *find, char *str);
+
+int main(){
+char str[] = {"I love to program"}, findstr[100];
+cout << str;
+cout << '\n' << "Enter find word: ";
+cin >> findstr;
+ char *res;
+res = f(findstr, str);
+cout << "Find string: " << res << endl;
 return 0;
 }
 
+
+char *f(char *find, char *str){
+char *p1, *p2, *start;
+for(int i=0;*str;i++){
+	p1 = &str[i];
+	start = p1;
+	p2 = find;
+while(*p2 && *p2==*start){
+	p1++;
+	p2++;
+}
+if(!*p2) return start;
+}
+return 0;
+}
