2cda60b83089dfbe4db814e733f4207bfcf905bd
Ivan <boss.marzan@bk.ru>
2021-07-17 17:33:12 +0300

    Альтернатива функции strcat с помощью аргуметов по умолчанию

--- Program.cpp
+++ Program.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
-void clear(int n = 25);
+void f(char *p1, char *p2, int n = -1);
 
 int main(){
-clear(40);/*Очистка экрана*/
+char test1[100] = "This is", test2[100] = " test.";
+cout << "You look result function form include <cstring>: " << strcat(test1,test2) << endl;
+strcpy(test1,"This is");
+f(test1, test2, 5);
+cout << "You look result my function: " << test1;
 return 0;
 }
 
-void clear(int n){
-for(;n>0;n--)cout << endl;
-	cout << "Screen cleared" << endl;
+void f(char *p1, char *p2, int n){
+while(*p1)p1++;
+if(n==-1) n = strlen(p2);
+while(*p2 && n){
+*p1 = *p2;
+p1++;p2++;
+n--;
+}
+*p1 = '\0';
 }
