a86f698b2f98dbf3c5565c2a678a29bb8609bfcb
Ivan <boss.marzan@bk.ru>
2021-07-20 09:31:44 +0300

    Выделение частей пятизначного числа

--- Program.cpp
+++ Program.cpp
@@ -2,24 +2,12 @@
 
 using namespace std;
 
-int f(register int x, register int y);
-
 int main(){
-	int a, b;
-cout << "Enter value A:";
-cin >> a;
-cout << "Enter value B:";
-cin >> b;
-cout << '\n' << f(a,b);
+int x;
+cout << "Enter X: ";
+cin >> x;
+if(x<100000 && x>9999)
+	for(int i=100000,j=1;i>9;i=i/10,j++)
+		cout << "Number [" << j << "] = " << (x%i)/(i/10) << endl;
 return 0;
-}
-
-int f(register int x, register int y){
-int sing;
-register int temp;
-if(x<0) sing = -1;
- else sing = 1;
-temp = 1;
-for(;y;y--) temp = temp * x;
-return temp * sing;
 }
