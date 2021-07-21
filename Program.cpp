7592e9e93f4d29e39779d8a1cb93b0b1942aa9a3
Ivan <boss.marzan@bk.ru>
2021-07-21 12:35:12 +0300

    Обнуление шестого бита (t=T)

--- Program.cpp
+++ Program.cpp
@@ -2,12 +2,32 @@
 
 using namespace std;
 
+void CC(char str);
+
 int main(){
-int x;
-cout << "Enter X: ";
-cin >> x;
-if(x<100000 && x>9999)
-	for(int i=100000,j=1;i>9;i=i/10,j++)
-		cout << "Number [" << j << "] = " << (x%i)/(i/10) << endl;
+char letter;
+do{
+cin >> letter;
+CC(letter);
+cout << letter << "& 11011111(223) = ";
+letter = letter & 223;
+CC(letter);
+}while(letter!='Q');
+
 return 0;
+}
+
+void CC(char str){
+	register int i, ost, chislo[15], j = 0;
+	i = (int) str;
+	cout << str << " ANSI code = " << (int) str;
+do{
+ost = i%2;
+i = i/2;
+chislo[j] = ost;
+j++;
+}while(i);
+cout << " 2CC = ";
+for(int i=j-1;i>=0;i--) cout << chislo[i] << ' ';
+cout << endl;
 }
