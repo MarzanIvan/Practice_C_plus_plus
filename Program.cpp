c69923e5afbceb8cc08c6a8640a193b1302285e7
Ivan <boss.marzan@bk.ru>
2021-07-29 12:26:02 +0300

    Нахождение подстроки с указанием длины выводимой части стори

--- Program.cpp
+++ Program.cpp
@@ -3,18 +3,25 @@
 
 using namespace std;
 
+char *f(char *p, char str);
+
 int main(){
-register char *p, *p1;
-int count = 0;
+register char *p, str;
+int len;
+cout << "Enter something in English: ";
 gets(p);
-p1 = p;
-while(*p1){
-  if(*p1!=' '){
-    while(*p1!=' ')p1++;
-    count++;
-  } 
-p1++;
-};
-cout << count;
+cout << '\n' << "Enter initial letter: ";
+cin >> str;
+cout << "Enter length: ";
+cin >> len;
+p = f(p, str);
+cout << "Your string: ";
+for(int i=0;i<len;i++,p++) cout << *p; 
 return 0;
+}
+
+char *f(char *p, char str){
+char *p1;
+for(p1=p;*p1;p1++) if(*p1==str)break;
+  return p1;
 }
