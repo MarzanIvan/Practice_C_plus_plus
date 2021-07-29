8a57d39bcba6cdd264675e7bc40927edcd282638
Ivan <boss.marzan@bk.ru>
2021-07-29 15:59:29 +0300

    Произведение двух матриц

--- Program.cpp
+++ Program.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
-#include <cstdio>
 
 using namespace std;
 
-char *f(char *p, char str);
-
 int main(){
-register char *p, str;
-int len;
-cout << "Enter something in English: ";
-gets(p);
-cout << '\n' << "Enter initial letter: ";
-cin >> str;
-cout << "Enter length: ";
-cin >> len;
-p = f(p, str);
-cout << "Your string: ";
-for(int i=0;i<len;i++,p++) cout << *p; 
-return 0;
+int m1,n1, m2,n2;
+cout << "First array, enter amount lines and columns: ";
+cin >> m1 >> n1;
+cout << "Second array, enter amount lines and columns: ";
+cin >> m2 >> n2;
+if(n1==n2|m1==m2){
+register int lines = m1, columns = n1, i, j, r, temp1, temp2, temp = 0;
+int **mass1 = new int*[lines], **mass2 = new int*[lines], **c = new int*[lines];
+for(int i=0;i<lines;i++) mass1[i] = mass2[i] = c[i] =  new int[columns];
+for(i=0;i<lines;i++){for(j=0;j<columns;j++){mass1[i][j] = mass1[i][j] = rand() % 10;}}
+cout << "Array A: " << endl;
+for(i=0;i<lines;i++){for(j=0;j<columns;j++){cout << mass1[i][j] << ' ';}cout << '\n';}
+cout << '\n' << "Array B: " << endl;
+for(i=0;i<lines;i++){for(j=0;j<columns;j++){cout << mass2[i][j] << ' ';}cout << '\n';}
+for(r=0;r<lines;r++){
+for(j=0;j<columns;j++){
+for(i=0;i<lines;i++){
+temp = (temp1 = mass1[r][i], temp2 = mass2[i][j], temp1 * temp2);
+cout <<  mass1[r][i] << " * " << mass2[i][j] << " = " << temp << " | ";
 }
-
-char *f(char *p, char str){
-char *p1;
-for(p1=p;*p1;p1++) if(*p1==str)break;
-  return p1;
+cout << endl;
+temp = (c[r][j] = temp, 0);
+}
+}
+cout << '\n' << "A * B: " << endl;
+for(i=0;i<lines;i++){for(j=0;j<columns;j++){cout << c[i][j] << ' ';}cout << '\n';}
+}
+return 0;
 }
