042179984fa979333c59803ad363053143288aac
Ivan <boss.marzan@bk.ru>
2021-08-03 17:55:46 +0300

    Find number quater plane

--- Program.cpp
+++ Program.cpp
@@ -3,13 +3,11 @@
 using namespace std;
 
 int main(){
-  register double x; register int c, m, a, y;
-  cout << "Enter A (0 <= A < M): "; cin >> a;
-  cout << "Enter Y (0 <= Y < M): "; cin >> y;
-  cout << "Enter M (M >= A && Y && 2): "; cin >> m;
-for(c=0;c<=m;c++){
-x = (y = (a * y + c) % m, (double)(y + 1) / m);
-cout << '[' << y << "] = " << x << endl;
-}
+int x, y;
+cout << "Enter x: "; cin >> x;
+cout << "Enter y: "; cin >> y;
+if(!x|!y) return 0;
+if(x<0)if(y>0)cout <<"Second";else cout <<"Third"; else if(y>0)cout <<"First";else cout <<"Fourth";
+cout << " quater plane";
 return 0;
 }
