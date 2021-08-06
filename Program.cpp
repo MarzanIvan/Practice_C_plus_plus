577606fb95c43e6a735a7bbb73279de547471ac4
Ivan <boss.marzan@bk.ru>
2021-08-06 16:46:25 +0300

    Правка

--- Program.cpp
+++ Program.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main(){
 float a, b, h;
 cout << "Enter initial values A, B, H: "; cin >> a >> b >> h;
-cout << "y = ";for(register float i=a;i<=b;i=i+h){cout << pow(cos(i)/sin(i),2)*log(i) << "; ";}
+cout << "y = ";for(register float i=a;i<=b;i=i+h){cout << pow(cos(log(i))/sin(log(i)),2) << "; ";}
 return 0;
 }
