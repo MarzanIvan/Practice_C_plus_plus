81edf45f3b73d7e13e1f95ea8adc3be4b0a17a74
Ivan <boss.marzan@bk.ru>
2021-08-06 16:25:54 +0300

    Является ли натуральное число палиндромом

--- Program.cpp
+++ Program.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
 int main(){
-int choose, higt, width; char ch; register i, j, c, t;
-cout << "Choose symbol: \n" << "1. *\n" << "2. +\n" << "3. #\n" << endl;cout << "Your choose: "; cin >> choose;
-switch (choose){
-	case 1: ch = '*'; break; case 2: ch = '+'; break; case 3: ch = '#'; break;
-	default: {cout << "Don't found"; return 0;}
-}
-cout << "Enter higt: "; cin >> higt;
-cout << "Enter width: "; cin >> width;
-for(i=1,j=c=(width/2)+1;i<=higt;i++){
-for(t=1;t<=width;t++)if(t==j||t==c) cout << ch; else cout << ' ';
-if(i<(width/2)+1){j--;c++;}else{j++;c--;}
-cout << endl;
-}
+char *p; cout << "Enter value below hundred thousand: "; cin >> p;
+for(register int i=0,j=strlen(p)-1;i<((int)strlen(p)/2);i++,j--){if(p[i]!=p[j]){cout << "Value isn't palinbrome!";return 0;}
+}cout << "Value is palindrome!";
 return 0;
 }
