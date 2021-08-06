b64fc9e29e476b689bfa563601247b3aec3e0a97
Ivan <boss.marzan@bk.ru>
2021-08-06 18:10:45 +0300

    Программа тест (without switch == Chinese wall)

--- Program.cpp
+++ Program.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
-#include <cmath>
-
+#include <cstring>
 using namespace std;
 
+const int size = 3;
+
 int main(){
-float a, b, h;
-cout << "Enter initial values A, B, H: "; cin >> a >> b >> h;
-cout << "y = ";for(register float i=a;i<=b;i=i+h){cout << pow(cos(log(i))/sin(log(i)),2) << "; ";}
+register int i, total = 0, choose = 0;
+char names[4][100] = {" About math", " About animals" , " Present Continuons" , " Exit"},
+	quest[size][100] = {"6 * ? = 12 (Answer∈N)","How many ears have human (Answer∈N)?","He's eat.. porridge"},
+	answers[size][100] = {"2","2","ing"}, answ[10];
+while(choose!=4){
+for(i=0;i<4;i++) cout << i+1 << "." << names[i] << endl;
+cout << "Choose questions: "; cin >> choose;
+if(choose==4) break;
+if(names[choose-1][0]=='*'){cout << "This is task is complete" << endl;continue;}
+cout << quest[choose-1] << '\n' << "Your answers: ";cin >> answ; 
+if(!strcmp(answ,answers[choose-1])){cout << "Good!" << endl;names[choose-1][0] = (total+=3, '*');} 	
+}
+cout << "Amout balls = " << total;
 return 0;
 }
