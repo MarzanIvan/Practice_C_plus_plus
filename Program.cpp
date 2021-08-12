d66322a0ed4935f9e5fd162ac6a6b7b575d4d527
Ivan <boss.marzan@bk.ru>
2021-08-12 19:18:49 +0300

    Static

--- Program.cpp
+++ Program.cpp
@@ -1,41 +1,18 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
-float findgip(float a, float b);
-float f(float a, float b);
-int f1(int a);
-bool f2(int a);
-void show(float mass[10]);
-enum status{good, broke} PC;
-void energy(double energ, status &PC);
+void f(float speed = 0, float time = 0);
 
 int main(){
-	for(int i=0;i<10;i++){
-	energy(rand()%320 ,PC);
-	if(PC==broke){cout << i << " PC is broke!"; break;}	
-	}
-/*
-cout << "Enter value: "; cin >> a;
-if(f2(a)) cout << a << "%2 = 0";
-
-float AC, BC, GIP; cout << "Enter AC, BC: "; cin >> AC >> BC; 
-GIP = findgip(AC, BC); cout << "Hypotinuse = " << GIP;
-*/
+f(13,2);
+f(25,10);
+f(45,4);
 return 0;
 }
 
-void energy(double energ, status &PC){
-	if(energ>310) {PC = broke; cout << energ << endl;}	
+void f(float speed, float time){
+	static int a;
+cout << "You went " << speed * time << "\nEarly S aqued " << a << endl;
+a = speed * time;
 }
-
-
-void show(float mass[10]){for(int i=0;i<10;i++) cout << mass[i] << ' ';}
-
-bool f2(int a){return !(a%2) ? 1 : 0;}
-
-int f1(int a){int i, j;for(i=1,j=1;i<=a;j=i*j,i++);return j;}
-
-float f(float a, float b){return a + a/100 * b;}
-
-float findgip(float a, float b){return sqrt(a*a + b*b);}
