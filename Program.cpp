c756a91abfe1e597bf58ef01e927734f68510b0e
Ivan <boss.marzan@bk.ru>
2021-08-17 11:22:30 +0300

    Work with pointer of struct

--- Program.cpp
+++ Program.cpp
@@ -2,17 +2,20 @@
 
 using namespace std;
 
-void f(float speed = 0, float time = 0);
+enum ocenk{good, bad};
+struct revise{ int trouble; ocenk ball; }students[3];
+
+int f(revise *p){
+int temp;
+for(temp=0;p->trouble;p++) if(p->ball==good) temp++;
+return temp;	
+}
 
 int main(){
-f(13,2);
-f(25,10);
-f(45,4);
+register int i; revise *p;
+for(p = students,i=1;i<=3;i++,p++){cout << "Trouble of student [" << i << "]: ";cin >> p->trouble;}
+for(p = students;p->trouble;p++)p->ball = p->trouble>=2?bad:good;	
+	cout << "Amount a good students aqued " << f(p = students) << endl;
 return 0;
 }
 
-void f(float speed, float time){
-	static int a;
-cout << "You went " << speed * time << "\nEarly S aqued " << a << endl;
-a = speed * time;
-}
