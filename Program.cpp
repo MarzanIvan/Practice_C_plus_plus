07ac5a9c1d515afcd4828210d43f7aaf208dd98e
Ivan <boss.marzan@bk.ru>
2021-10-01 21:46:59 +0300

    y = a*x^2 + b*x + c; y = d*x + k if D>0 result[value;value]

--- Program.cpp
+++ Program.cpp
@@ -1,50 +1,44 @@
 #include <iostream>
-#include <cctype>
-
+#include <cmath>
 using namespace std;
 
-class Array{
-   double *array;
-   int size;
-   public:
-   Array();
-   ~Array(){ delete[] array; }
-   double *getarray(){ return array;}
-   void generate(){ Array();}
-   double min();
-   void sort();
-   void show();
-   double max(); 
-};
-
-void Array::sort(){
-    
+inline double f(int a, int b, double D){
+return - b + sqrt(D) / (2 * a);
+}
+inline double f1(int a, int b, double D){
+return - b - sqrt(D) / (2 * a);
 }
 
-double Array::min(){
-    double *p = array, min = *p;
-    for(int i=0;i<size;p++,i++)if(*p<min) min = *p;
-    return min;
-}
-double Array::max(){
-    double *p = array, max = *p;
-    for(int i=0;i<size;p++,i++)if(*p>max) max = *p;
-    return max;
+inline void task(int a, int b, int c,int d,int k, double D, double x){
+    double y;
+    y = a * x * x + b * x + c;
+    cout << "Enter D, K: "; cin >> d >> k;
+    x = y / (d + k);
+    y = d * x + k;
+    cout << "Result = [" << x << ';' << y << ']' << endl;
 }
 
-Array::Array(){
-    double *p;
-    cout << "Enter amount elements of the array: ";
-    cin >> size;
-    array = new double[size]; p = array;
-    for(int i=0;i<size;p++,i++){
-        *p = 1 + rand() % 10;
-        cout << *p << ' ';
-    } cout << '\n';
+void input(){
+int a, b, c, d, k;
+double x, D;
+cout << "Enter A, B, C: ";cin >> a >> b >> c;
+D = b*b - 4 * a * c;
+if(D>0){
+    cout << "D > 0" << endl;
+    x = f(a,b,D);
+    task(a,b,c,d,k,D,x);
+    x = f1(a,b,D);
+    task(a,b,c,d,k,D,x);
+} else if(!D){
+    cout << "D = 0" << endl;
+x = f(a,b,D);
+task(a,b,c,d,k,D,x);
+} else {
+    cout << "D < 0!";
+}
 }
 
 int main(){
-Array obj;
-cout << "Minimum = " << obj.min() << '\n' << "Maximum = " << obj.max() << endl;
+input();
 return 0; 
 }
