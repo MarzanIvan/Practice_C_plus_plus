4acd50d9688f39e986e2bf14f1df28a574f403f7
Ivan <boss.marzan@bk.ru>
2021-10-04 21:30:39 +0300

    The typical timer

--- Program.cpp
+++ Program.cpp
@@ -1,44 +1,31 @@
 #include <iostream>
-#include <cmath>
+#include <ctime>
+
 using namespace std;
 
-inline double f(int a, int b, double D){
-return - b + sqrt(D) / (2 * a);
-}
-inline double f1(int a, int b, double D){
-return - b - sqrt(D) / (2 * a);
-}
+class timer{
+int seconds;
+public:
+timer(int InSecond){ seconds = InSecond; }
+timer( int sec, int min,  int hour = 0){ min += hour * 60; seconds = sec + min * 60; }
+void start();
+};
 
-inline void task(int a, int b, int c,int d,int k, double D, double x){
-    double y;
-    y = a * x * x + b * x + c;
-    cout << "Enter D, K: "; cin >> d >> k;
-    x = y / (d + k);
-    y = d * x + k;
-    cout << "Result = [" << x << ';' << y << ']' << endl;
+void timer::start(){
+clock_t tm;
+while(seconds>=0){
+tm = clock() / CLOCKS_PER_SEC; // Получаем количество секунд работы процессора относительно этого момента
+while(clock() / CLOCKS_PER_SEC - tm < 1); // Выполнение ожидания, равное одной секунде
+cout << seconds << "..." << endl;
+    seconds--;
 }
-
-void input(){
-int a, b, c, d, k;
-double x, D;
-cout << "Enter A, B, C: ";cin >> a >> b >> c;
-D = b*b - 4 * a * c;
-if(D>0){
-    cout << "D > 0" << endl;
-    x = f(a,b,D);
-    task(a,b,c,d,k,D,x);
-    x = f1(a,b,D);
-    task(a,b,c,d,k,D,x);
-} else if(!D){
-    cout << "D = 0" << endl;
-x = f(a,b,D);
-task(a,b,c,d,k,D,x);
-} else {
-    cout << "D < 0!";
-}
+cout << "Timer stopped" << endl;
 }
 
 int main(){
-input();
-return 0; 
+    int seconds;
+    cout << "Enter an amount of the seconds: "; cin >> seconds; 
+timer objtimer = seconds; // вызываем перегружаемый конструктор, принимающий только int значения
+objtimer.start(); // Включение таймера
+return 0;
 }
