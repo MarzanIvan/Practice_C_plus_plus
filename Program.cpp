81a4964098bc4b648ac7c8f93c4352c1114f4a61
Ivan <boss.marzan@bk.ru>
2021-11-03 16:03:36 +0300

    Created class has a lot of the operator functions. There is an indexing of the string, a conversion two string, a decrease two string, deleting char fragment of string, assignmenting string. The all members of the class are pointers

--- Program.cpp
+++ Program.cpp
@@ -1,31 +1,13 @@
 #include <iostream>
-#include <ctime>
+#include "Moduls/classes.h"
 
 using namespace std;
 
-class timer{
-int seconds;
-public:
-timer(int InSecond){ seconds = InSecond; }
-timer( int sec, int min,  int hour = 0){ min += hour * 60; seconds = sec + min * 60; }
-void start();
-};
-
-void timer::start(){
-clock_t tm;
-while(seconds>=0){
-tm = clock() / CLOCKS_PER_SEC; // Получаем количество секунд работы процессора относительно этого момента
-while(clock() / CLOCKS_PER_SEC - tm < 1); // Выполнение ожидания, равное одной секунде
-cout << seconds << "..." << endl;
-    seconds--;
-}
-cout << "Timer stopped" << endl;
-}
-
 int main(){
-    int seconds;
-    cout << "Enter an amount of the seconds: "; cin >> seconds; 
-timer objtimer = seconds; // вызываем перегружаемый конструктор, принимающий только int значения
-objtimer.start(); // Включение таймера
-return 0;
+    TypeString Fragment("LL");
+    char *String = new char[20];
+    strcpy(String, "HELLO");
+    String - Fragment;
+    cout << String;    
+    return 0;
 }
