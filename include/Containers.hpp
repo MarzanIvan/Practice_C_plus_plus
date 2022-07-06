c6ee7a014084c17e04ce61afd21f23985d5aba3e
Ivan <boss.marzan@bk.ru>
2022-07-06 19:38:13 +0300

    Correct the class Item to be able to switch on next or previous item

--- include/Containers.hpp
+++ include/Containers.hpp
@@ -2,23 +2,36 @@
 
 template <class TypeItem>
 struct Item {
-    TypeItem *NextObject;
+    Item *NextItem;
     TypeItem *CurrectObject;
-    TypeItem *PreviousObject;
+    Item *PreviousItem;
     Item(TypeItem *ObjectToContain = nullptr) {
         CurrectObject = ObjectToContain;
-        NextObject = PreviousObject = nullptr;
+        NextItem = PreviousItem = nullptr;
     }
 };
 
 template <class TypeItems>
 class Container {
 protected:
-    Item<TypeItems> *Items[];
+    Item<TypeItems> *StartElement;
+    Item<TypeItems> *EndElement;
     unsigned int AmountItems;
 public:
-    Container( TypeItems *ObjectsToContain = nullptr ) {
-        
+    Container( TypeItems *ObjectToContain = nullptr ) {
+        StartElement = new Item<TypeItems>(ObjectToContain);
+        EndElement = StartElement;
+        if (!ObjectToContain) {
+            AmountItems = 0;
+        } else {
+            AmountItems = 1;
+        }
+    }
+    Item<TypeItems> *Container::operator[]( unsigned int IndexItem ) {
+        TypeItems ItemToGet = ;
+        for ( unsigned int i = 0; i < IndexItem; i++ ) {
+            
+        }
     }
 
 };
